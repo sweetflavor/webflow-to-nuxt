1e191c01ea47581b8fd080c3e4d2601091579ce9
sweetflavor <sweet.devtools@gmail.com>
2020-11-13 00:38:25 +0100

    Adding pages dir

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,11 @@
 import argOptions from './options'
-import createDir from './utils'
+import { createDir, cdInToDir } from './utils'
 
 export function cli(args) {
 	const options = argOptions(args);
 	const name = options.name
 	const path = options.path
 	createDir(name)
+	cdInToDir(name)
+	createDir('pages')
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,7 @@ const { exec } = require("child_process")
 export default function createDir(name) {
     exec(`mkdir ${name}`)
 }
+
+export default function cdInToDir(name) {
+    exec(`cd ${name}/`)
+}
