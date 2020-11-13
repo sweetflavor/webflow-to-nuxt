16680dbaaec78e3b9708210e4df1feece361a7a0
Alex Man <sweet.devtools@gmail.com>
2020-11-13 02:57:37 +0100

    Create page dir with vue files inside

--- src/create-nuxt/index.js
+++ src/create-nuxt/index.js
@@ -1,12 +1,19 @@
 const { exec } = require("child_process")
+const execa = require('execa');
 const fs = require('fs');
 
 export default function createNuxtProject (name, path) {
     // exec(`mkdir ${name} && cd ${name}/ && mkdir pages && cd pages/`)
     // echo "<template></template>" >index.vue
+    (async () => {
+        const { stdout } = await exec(`mkdir ${name} && cd ${name} && mkdir pages && cd pages/`);
+    })();
 
-    
     fs.readdir(path, (err, files) => {
         console.log(files);
+        files.map(file => {
+            console.log(file);
+            exec(`cd ${name}/pages && echo "<template></template>" > ${file}.vue`)
+        })
     })
 }
