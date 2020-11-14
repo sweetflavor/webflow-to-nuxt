e8d3ec9b5d68b21d7642d0be375e2f95556ab8d9
sweetflavor <sweet.devtools@gmail.com>
2020-11-14 01:51:03 +0100

    Adding package.json in to the nuxt project.

--- src/create-nuxt/index.js
+++ src/create-nuxt/index.js
@@ -3,12 +3,11 @@ const execa = require('execa');
 const fs = require('fs');
 
 export default function createNuxtProject (name, path) {
-    // exec(`mkdir ${name} && cd ${name}/ && mkdir pages && cd pages/`)
-    // echo "<template></template>" >index.vue
+    // Create basic nuxt structure
     (async () => {
         const { stdout } = await exec(`mkdir ${name} && cd ${name} && mkdir pages && cd pages/`);
     })();
-
+    // Create pages into page/ dir
     fs.readdir(path, (err, files) => {
         console.log(files);
         files.map(file => {
@@ -16,4 +15,9 @@ export default function createNuxtProject (name, path) {
             exec(`cd ${name}/pages && echo "<template></template>" > ${file}.vue`)
         })
     })
+    // Create package.json
+    const packageFile = `{"name": "my-app","scripts": { "dev": "nuxt", "build": "nuxt build", "generate": "nuxt generate", "start": "nuxt start" }}`
+
+    exec(`cd ${name}/pages && echo "${JSON.stringify(packageFile)}" > package.json && npm run install`)
+   
 }
