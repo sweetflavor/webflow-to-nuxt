f12f528dea0956d12d72da73683c0d24f61ea985
Alex Man <sweet.devtools@gmail.com>
2020-11-16 01:10:36 +0100

    Adding package info

--- src/create-nuxt/create-nuxt-package-file/index.js
+++ src/create-nuxt/create-nuxt-package-file/index.js
@@ -1,6 +1,6 @@
 const { exec } = require("child_process")
 
 export default function createNuxtPackageFile (name) {
-    const packageFile = `{"name": "my-app","scripts": { "dev": "nuxt", "build": "nuxt build", "generate": "nuxt generate", "start": "nuxt start" }}`
-    exec(`cd ${name}/pages && echo "${JSON.stringify(packageFile)}" > package.json`)
+    const packageFile = `{"name": "${name}", "version": "0.0.1", "private": true, "license": "MIT", "scripts": { "dev": "nuxt", "build": "nuxt build", "generate": "nuxt generate", "start": "nuxt start" }}`
+    exec(`cd ${name}/ && echo "${JSON.stringify(packageFile)}" > package.json`)
 }
--- src/create-nuxt/create-nuxt-pages/index.js
+++ src/create-nuxt/create-nuxt-pages/index.js
@@ -2,11 +2,29 @@ const { exec } = require("child_process")
 const fs = require('fs');
 
 export default function createNuxtPages (name, path) {
+    
     fs.readdir(path, (err, files) => {
         console.log(files);
         files.map(file => {
             console.log(file);
-            exec(`cd ${name}/pages && echo "<template></template>" > ${file.replace('.html', '.vue')}`)
+            const baseVueStructure = new String(`
+                        <template>
+                            <div>
+                                <h1>{{ msg }}</h1>
+                            </div>
+                        </template>
+                        <script>
+                        export default {
+                            name: '${file.replace('.html', '')}',
+                            data () {
+                                return {
+                                    msg: 'Welcome to the ${file.replace('.html', '')} page!'
+                                }
+                            }
+                        }
+                        </script>
+    `)
+            exec(`cd ${name}/pages && echo "${baseVueStructure}" > ${file.replace('.html', '.vue')}`)
         })
     })
 }
